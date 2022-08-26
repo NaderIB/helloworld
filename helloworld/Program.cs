f6bb3df18964a7967108579cca3afb55644b845e
nader <nader@LAPTOP-6BSTED93>
2022-08-26 18:29:50 -0400

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace hellowworld_2
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            string stg = "Hello world! (2)";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter users age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Username is: " +username + " and the age is: " + age);
+        }
+    }
+}
